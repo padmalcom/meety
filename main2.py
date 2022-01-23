c19683d102c95e7a69d87b68dd0ef2c7ef0a457b
padmalcom <j.freiknecht@googlemail.com>
2022-01-23 20:30:42 +0100

    speaker detection

--- main2.py
+++ main2.py
@@ -1,57 +1,85 @@
 import vosk
-import sys
-import os
 import wave
 import json
-import datetime
+from pydub import AudioSegment
+from datetime import datetime
+import os
 
-WORDS_PER_LINE = 7
-
-def transcribe():
-
-	vosk.SetLogLevel(-1)
-
-	audio_path = "meeting.wav"
-	out_path = "out.csv"
-
-	model_path = 'vosk-model-small-de-0.15'
-	#model_path = 'vosk-model-de-0.21'
-	spk_model_path = 'vosk-model-spk-0.4'
-	sample_rate = 16000
+class transcriber:
 	
-	model = vosk.Model(model_path)
-	spk_model = vosk.SpkModel(spk_model_path)
-	rec = vosk.KaldiRecognizer(model, sample_rate)
-	rec.SetSpkModel(spk_model)
-	rec.SetWords(True)
-	
-	wf = wave.open(audio_path, "rb")
-	
-	results = []
-	subs = []
-	while True:
-	   data = wf.readframes(4000)
-	   if len(data) == 0:
-		   break
-	   if rec.AcceptWaveform(data):
-		   results.append(rec.Result())
-	results.append(rec.FinalResult())
+	def __init__(self):
+		vosk.SetLogLevel(-1)
+		model_path = 'vosk-model-small-de-0.15'
+		#model_path = 'vosk-model-de-0.21'
+		spk_model_path = 'vosk-model-spk-0.4'
+		sample_rate = 16000
+		model = vosk.Model(model_path)
+		spk_model = vosk.SpkModel(spk_model_path)
+		self.rec = vosk.KaldiRecognizer(model, sample_rate)
+		self.rec.SetSpkModel(spk_model)
+		self.rec.SetWords(True)
+		
 
-	for i, res in enumerate(results):
-		print("Result " + str(i) + ": " + str(res))
+	def cosine_dist(self, x, y):
+		nx = np.array(x)
+		ny = np.array(y)
+		return 1 - np.dot(nx, ny) / np.linalg.norm(nx) / np.linalg.norm(ny)
+		
+	def get_speaker_from_split(self, audio_file, start, stop):
+		audio = AudioSegment.from_wav(audio_file)
+		excerpt = audio[start*1000:stop*1000]
+		timestamp = datetime.now().microsecond
+		excerpt.export(str(timestamp) + ".wav", format='wav')
+		wf = wave.open(str(timestamp) + ".wav", "rb")
+		while True:
+		   data = wf.readframes(4000)
+		   if len(data) == 0:
+			   break
+		   if self.rec.AcceptWaveform(data):
+			   self.rec.Result()
+		res = self.rec.FinalResult()
 		jres = json.loads(res)
-		if not 'result' in jres:
-			continue
-		words = jres['result']
-		for j in range(0, len(words)):
-			line = words[j] 
-			print("Line: " + str(line))
-			#s = srt.Subtitle(index=len(subs), 
-			#	content=" ".join([l['word'] for l in line]),
-			#	start=datetime.timedelta(seconds=line[0]['start']), 
-			#	end=datetime.timedelta(seconds=line[-1]['end']))
-			#subs.append(s)
-	return subs
+		wf.close()
+		os.remove(str(timestamp) + ".wav")
+		if 'spk' in jres:
+			return jres['spk']
+		else:
+			return []
+		
+
+	def transcribe(self, audio_file, csv_output):
+	
+		wf = wave.open(audio_file, "rb")
+		
+		results = []
+		while True:
+		   data = wf.readframes(4000)
+		   if len(data) == 0:
+			   break
+		   if self.rec.AcceptWaveform(data):
+			   results.append(self.rec.Result())
+		results.append(self.rec.FinalResult())
+
+		subs = []
+		for i, res in enumerate(results):
+			print("Result " + str(i) + ": " + str(res))
+			jres = json.loads(res)
+			if not 'result' in jres:
+				continue
+			words = jres['result']
+			for j in range(0, len(words)):
+				line = words[j] 
+				print("Line: " + str(line))
+				#s = srt.Subtitle(index=len(subs), 
+				#	content=" ".join([l['word'] for l in line]),
+				#	start=datetime.timedelta(seconds=line[0]['start']), 
+				#	end=datetime.timedelta(seconds=line[-1]['end']))
+				#subs.append(s)
+				speaker = self.get_speaker_from_split(audio_file, line['start'], line['end'])
+				print(line['word'] + " " + str(line['conf']) + " " + str(line['start']) + " " + str(line['end']) + " " + str(speaker))
+		return subs
 	
 if __name__ == '__main__':
-	print(transcribe())
+	t = transcriber()
+	transcription = t.transcribe('meeting.wav', 'out.csv')
+	print(transcription)
