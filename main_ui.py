b4d4781c0c5d4d3a64c5af6bb994eef7d95b2ccc
padmalcom <j.freiknecht@googlemail.com>
2022-02-03 12:57:10 +0100

    Annoying fixes

--- main_ui.py
+++ main_ui.py
@@ -63,7 +63,7 @@ class Transcriber:
 		else:
 			model_path = 'vosk-model-de-0.21'
 		spk_model_path = 'vosk-model-spk-0.4'
-		self.COSINE_DIST = 0.8
+		self.COSINE_DIST = 0.5
 		self.UNDETECTED_SPEAKER = "undetected speaker"
 		self.UNKNOWN_SPEAKER = "unknown speaker"
 		self.sample_rate = 16000
@@ -72,7 +72,8 @@ class Transcriber:
 		self.rec = vosk.KaldiRecognizer(self.model, self.sample_rate, self.spk_model)
 		
 		self.speakers={}
-		#self.speakers["jonas"] = [-0.849626, 0.1394, 0.377193, 0.04026, -0.516814, 0.684988, 0.276028, 0.44519, -0.071336, 0.236369, 1.438427, -1.16203, 0.171904, -0.246187, 1.162354, 1.754254, -0.870339, 0.581138, 0.277063, -0.213094, -0.178992, 1.307812, 0.383807, -1.808675, -0.183495, 0.595374, 0.552995, 1.130693, 1.509223, 0.55946, 0.377448, -0.874153, -0.805022, -0.053774, -0.024277, 0.100899, -0.16061, 0.979247, -0.806801, 1.075389, 0.054593, 0.457873, -0.123776, 0.102562, -1.03972, -0.405821, -2.296902, 1.552534, 1.987395, -0.178972, -1.699092, -1.007383, -0.044273, 0.668422, -0.387606, 1.032058, 1.218006, -0.004968, -2.110302, -0.588267, -1.630525, -0.486805, -0.149978, -0.323995, 0.057739, -1.591965, -0.507674, 2.15651, 0.087731, 0.380945, 1.659711, 1.078468, -0.96696, 1.021768, -0.429574, 1.221862, -1.227425, -0.760518, 0.883041, 1.018671, 1.070466, -0.274897, 0.163634, -0.747442, -0.05772, 0.477834, -2.581385, 0.522382, -1.506052, 1.325543, -0.888436, -0.042877, -0.999906, -0.163342, -0.045787, -2.195781, 1.082597, -0.170146, -0.511741, 0.051157, -1.417514, -1.123549, 0.37979, -0.715364, 0.868087, 0.182235, 1.849722, 2.322327, 0.353243, 0.963822, -1.506676, 1.008916, 0.883645, -0.744136, -0.141038, -0.231468, 0.424568, 0.640757, 1.176899, 0.703505, -1.75687, -0.789799, -0.169159, -1.980155, -0.769657, 0.923687, -0.850422, 1.737328]
+		self.speakers["jonas"] = [-0.849626, 0.1394, 0.377193, 0.04026, -0.516814, 0.684988, 0.276028, 0.44519, -0.071336, 0.236369, 1.438427, -1.16203, 0.171904, -0.246187, 1.162354, 1.754254, -0.870339, 0.581138, 0.277063, -0.213094, -0.178992, 1.307812, 0.383807, -1.808675, -0.183495, 0.595374, 0.552995, 1.130693, 1.509223, 0.55946, 0.377448, -0.874153, -0.805022, -0.053774, -0.024277, 0.100899, -0.16061, 0.979247, -0.806801, 1.075389, 0.054593, 0.457873, -0.123776, 0.102562, -1.03972, -0.405821, -2.296902, 1.552534, 1.987395, -0.178972, -1.699092, -1.007383, -0.044273, 0.668422, -0.387606, 1.032058, 1.218006, -0.004968, -2.110302, -0.588267, -1.630525, -0.486805, -0.149978, -0.323995, 0.057739, -1.591965, -0.507674, 2.15651, 0.087731, 0.380945, 1.659711, 1.078468, -0.96696, 1.021768, -0.429574, 1.221862, -1.227425, -0.760518, 0.883041, 1.018671, 1.070466, -0.274897, 0.163634, -0.747442, -0.05772, 0.477834, -2.581385, 0.522382, -1.506052, 1.325543, -0.888436, -0.042877, -0.999906, -0.163342, -0.045787, -2.195781, 1.082597, -0.170146, -0.511741, 0.051157, -1.417514, -1.123549, 0.37979, -0.715364, 0.868087, 0.182235, 1.849722, 2.322327, 0.353243, 0.963822, -1.506676, 1.008916, 0.883645, -0.744136, -0.141038, -0.231468, 0.424568, 0.640757, 1.176899, 0.703505, -1.75687, -0.789799, -0.169159, -1.980155, -0.769657, 0.923687, -0.850422, 1.737328]
+		self.speakers["fabian"] = [-0.797457, 0.893783, -0.458962, 0.439499, 0.121801, 0.269168, -0.605457, -0.07904, 1.278862, -0.47834, 2.048105, -1.236233, 0.979613, -0.267615, 0.159861, 0.055959, -0.256166, 1.260095, 2.314549, -2.207093, -0.710628, -0.170672, 0.320985, -0.492729, 0.395429, 0.310763, -0.650441, -0.726839, 1.783956, -0.514899, -0.771253, -1.798682, -1.846042, 0.154697, 0.979613, 0.138115, 0.229337, 0.092984, -0.169231, 0.498774, -1.416122, 0.110558, 0.30648, -0.467408, 0.496537, -2.26419, 0.428986, -1.017538, 1.711265, 0.492168, -0.702733, -0.271146, -0.805249, 0.425988, 1.871118, 0.590559, 0.250679, 0.898046, 1.574314, 1.043915, -0.886984, 0.12249, 1.196055, 1.362341, 0.738961, 1.07415, 0.256881, 1.817265, -0.546605, -0.569249, -0.469055, 0.563007, -1.593265, 1.247733, 1.711871, -0.320994, -0.566991, -2.293762, 1.308869, 1.3637, -0.170194, 0.640718, -0.016229, -0.805548, -0.511637, 0.785791, -1.111833, -0.521746, -0.939944, 1.332535, 0.793953, -0.004191, -0.06619, -1.196705, 0.919897, 0.844841, 0.471952, 0.559364, 1.94756, -1.286218, -0.334231, -2.265524, 0.673755, 0.234436, -0.412774, 0.885559, 1.116514, 2.089988, -1.357796, -0.271067, 0.642276, 0.949255, 0.347166, -0.594057, 1.056821, 0.960383, -1.361658, -0.283667, -0.764461, 0.674468, -0.529306, -0.359495, 0.656774, -0.703565, 0.369017, 0.894422, -1.642501, 0.963191]
 		self.new_speaker_index = 0
 		
 		punc_predict_path = os.path.abspath('vosk-recasepunc-de-0.21\\checkpoint')
@@ -138,9 +139,11 @@ class Transcriber:
 		res = self.rec.FinalResult()
 		jres = json.loads(res)
 		wf.close()
-		os.remove(str(timestamp) + ".wav")
+		#os.remove(str(timestamp) + ".wav")
 		if 'spk' in jres:
 			speaker_name, cd = self.get_speaker(jres['spk'])
+			logger.trace("Speaker fingerprint {}.", jres['spk'])
+			logger.info("Speaker for {} is {}.", str(timestamp) + ".wav", speaker_name)
 			return speaker_name, cd
 		else:
 			return self.UNDETECTED_SPEAKER, 1000
@@ -193,7 +196,8 @@ class Transcriber:
 				best_end_index = i
 				
 			# Experimental - don't compare sentences, if the length of the constructed length is twice the size
-			if len(repaired_sentence_without_punct) < len(sentence_to_compare) * 2:
+			if len(repaired_sentence_without_punct)*2 < len(sentence_to_compare):
+				logger.trace("Length of sentence to compare is twice as big - break.")
 				break
 		logger.debug("Best end {}, best ratio {}.", best_end_index, best_ratio)
 		return best_end_index		
@@ -289,6 +293,8 @@ class Transcriber:
 			audio = AudioSegment.from_mp3(audio_file)
 		elif file_extension.lower() == '.wav':
 			audio = AudioSegment.from_wav(audio_file)
+		elif file_extension.lower() == '.m4a':
+			audio = AudioSegment.from_file(audio_file)			
 		else:
 			logger.error("File format {} not supported.", file_extension)
 			sys.exit(0)
@@ -348,6 +354,8 @@ def create_transcript(temp_file_name, original_file_name, original_file_extensio
 		text += "- __[{}]__ Extracting full text from prepared file {}...  \n".format(timedelta(seconds=end-start), fixed_audio_file)
 		placeholder.info(text)
 	annotated_words, full_text = t.get_words_from_text(fixed_audio_file)
+	for aw in annotated_words:
+		print(aw)
 	
 	if show_log:
 		end = timer()
@@ -414,7 +422,7 @@ def create_transcript(temp_file_name, original_file_name, original_file_extensio
 		placeholder.info(text)
 	file_name, file_extension = os.path.splitext(audio_file)
 	
-	os.remove(fixed_audio_file)
+	#os.remove(fixed_audio_file)
 	
 	end = timer()
 	
@@ -430,7 +438,7 @@ def create_transcript(temp_file_name, original_file_name, original_file_extensio
 			
 if __name__ == '__main__':
 	logger.remove()
-	logger.add(sys.stderr, level="INFO")
+	logger.add(sys.stderr, level="TRACE")
 	
 	st.title("Meety - Herzlichen Willkommen!")
 	st.header("AI-basierte Audiotranskription für Web-Konferenzen")
@@ -447,8 +455,12 @@ if __name__ == '__main__':
 	temp_file_name = ""
 	original_file_name = ""
 	original_file_extension = ""
+	
+	st.markdown("Grad' kein Meetingmitschnitt zuhand? Lade einfach unsere kurze Demo!")
+	with open("demo.mp3", 'rb') as f:
+		st.download_button('Download demo.mp3', f, file_name="demo.mp3")
 		
-	uploaded_file = st.file_uploader("Wählen eine Audiodatei", type=['wav', 'mp3'])
+	uploaded_file = st.file_uploader("Wählen eine Audiodatei", type=['wav', 'mp3', 'm4a'])
 	if uploaded_file is not None:
 
 		# store file as temp file
