4453a3d41a97261c18ced56287617d3d72b0a2ff
padmalcom <j.freiknecht@googlemail.com>
2022-02-04 13:05:03 +0100

    Added subtitles and mp4 support

--- main_ui.py
+++ main_ui.py
@@ -20,6 +20,9 @@ from timeit import default_timer as timer
 from datetime import timedelta
 from transformers import pipeline
 
+from webvtt import WebVTT, Caption
+import textwrap
+
 import streamlit as st
 from stqdm import stqdm
 
@@ -63,7 +66,7 @@ class Transcriber:
 		else:
 			model_path = 'vosk-model-de-0.21'
 		spk_model_path = 'vosk-model-spk-0.4'
-		self.COSINE_DIST = 0.5
+		self.COSINE_DIST = 0.4
 		self.UNDETECTED_SPEAKER = "undetected speaker"
 		self.UNKNOWN_SPEAKER = "unknown speaker"
 		self.sample_rate = 16000
@@ -76,7 +79,7 @@ class Transcriber:
 		self.speakers["fabian"] = [-0.797457, 0.893783, -0.458962, 0.439499, 0.121801, 0.269168, -0.605457, -0.07904, 1.278862, -0.47834, 2.048105, -1.236233, 0.979613, -0.267615, 0.159861, 0.055959, -0.256166, 1.260095, 2.314549, -2.207093, -0.710628, -0.170672, 0.320985, -0.492729, 0.395429, 0.310763, -0.650441, -0.726839, 1.783956, -0.514899, -0.771253, -1.798682, -1.846042, 0.154697, 0.979613, 0.138115, 0.229337, 0.092984, -0.169231, 0.498774, -1.416122, 0.110558, 0.30648, -0.467408, 0.496537, -2.26419, 0.428986, -1.017538, 1.711265, 0.492168, -0.702733, -0.271146, -0.805249, 0.425988, 1.871118, 0.590559, 0.250679, 0.898046, 1.574314, 1.043915, -0.886984, 0.12249, 1.196055, 1.362341, 0.738961, 1.07415, 0.256881, 1.817265, -0.546605, -0.569249, -0.469055, 0.563007, -1.593265, 1.247733, 1.711871, -0.320994, -0.566991, -2.293762, 1.308869, 1.3637, -0.170194, 0.640718, -0.016229, -0.805548, -0.511637, 0.785791, -1.111833, -0.521746, -0.939944, 1.332535, 0.793953, -0.004191, -0.06619, -1.196705, 0.919897, 0.844841, 0.471952, 0.559364, 1.94756, -1.286218, -0.334231, -2.265524, 0.673755, 0.234436, -0.412774, 0.885559, 1.116514, 2.089988, -1.357796, -0.271067, 0.642276, 0.949255, 0.347166, -0.594057, 1.056821, 0.960383, -1.361658, -0.283667, -0.764461, 0.674468, -0.529306, -0.359495, 0.656774, -0.703565, 0.369017, 0.894422, -1.642501, 0.963191]
 		self.new_speaker_index = 0
 		
-		punc_predict_path = os.path.abspath('vosk-recasepunc-de-0.21\\checkpoint')
+		punc_predict_path = os.path.abspath('vosk-recasepunc-de-0.21/checkpoint')
 		self.casePuncPredictor = CasePuncPredictor(punc_predict_path, lang="de")
 		
 		#self.summarizer = pipeline("summarization", model="ml6team/mt5-small-german-finetune-mlsum", tokenizer="ml6team/mt5-small-german-finetune-mlsum")
@@ -139,7 +142,7 @@ class Transcriber:
 		res = self.rec.FinalResult()
 		jres = json.loads(res)
 		wf.close()
-		#os.remove(str(timestamp) + ".wav")
+		os.remove(str(timestamp) + ".wav")
 		if 'spk' in jres:
 			speaker_name, cd = self.get_speaker(jres['spk'])
 			logger.trace("Speaker fingerprint {}.", jres['spk'])
@@ -289,12 +292,16 @@ class Transcriber:
 	def fix_audio(self, audio_file):
 		file_name, file_extension = os.path.splitext(audio_file)
 		audio = None
+		is_video = False
 		if file_extension.lower() == '.mp3':
 			audio = AudioSegment.from_mp3(audio_file)
 		elif file_extension.lower() == '.wav':
 			audio = AudioSegment.from_wav(audio_file)
 		elif file_extension.lower() == '.m4a':
-			audio = AudioSegment.from_file(audio_file)			
+			audio = AudioSegment.from_file(audio_file)
+		elif file_extension.lower() == '.mp4':
+			audio = AudioSegment.from_file(audio_file)
+			is_video = True
 		else:
 			logger.error("File format {} not supported.", file_extension)
 			sys.exit(0)
@@ -305,7 +312,7 @@ class Transcriber:
 		timestamp = datetime.now().microsecond
 		file_name = "conv" + str(timestamp) + ".wav"
 		audio.export(file_name, format='wav', bitrate="64k")
-		return file_name
+		return file_name, is_video
 		
 	def write_docx(self, title, annotated_sentences, out_file):
 		document = Document()
@@ -330,6 +337,14 @@ class Transcriber:
 
 		document.save(out_file)
 		
+	def write_webvtt(self, annotated_sentences, out_file):
+		vtt = WebVTT()
+		for anse in annotated_sentences:
+			caption = Caption(anse.get_formated_start(), anse.get_formated_end(), textwrap.fill(anse.speaker + ": " + anse.sequence))
+			vtt.captions.append(caption)
+		vtt.save(out_file)
+		
+		
 	def summarize(self, text):
 		min_length = max(5, int(len(text) * 0.05))
 		max_length = min(200, int(len(text) * 0.3))
@@ -345,9 +360,17 @@ def create_transcript(temp_file_name, original_file_name, original_file_extensio
 	
 	if show_log:
 		end = timer()
-		text += "- __[{}]__ Preparing audio file... :speaker:  \n".format(timedelta(seconds=end-start))
+		text += "- __[{}]__ Preparing file...  \n".format(timedelta(seconds=end-start))
 		placeholder.info(text)
-	fixed_audio_file = t.fix_audio(audio_file)
+	fixed_audio_file, is_video = t.fix_audio(audio_file)
+	
+	if show_log:
+		end = timer()
+		if is_video:
+			text += "- __[{}]__ Oh look, it's a video! :tv:  \n".format(timedelta(seconds=end-start))
+		else:
+			text += "- __[{}]__ Oh look, it's an audio file! :loudspeaker:  \n".format(timedelta(seconds=end-start))
+		placeholder.info(text)
 	
 	if show_log:
 		end = timer()
@@ -422,7 +445,7 @@ def create_transcript(temp_file_name, original_file_name, original_file_extensio
 		placeholder.info(text)
 	file_name, file_extension = os.path.splitext(audio_file)
 	
-	#os.remove(fixed_audio_file)
+	os.remove(fixed_audio_file)
 	
 	end = timer()
 	
@@ -433,12 +456,19 @@ def create_transcript(temp_file_name, original_file_name, original_file_extensio
 	t.write_docx(original_file_name, sentences_with_speakers, docx_filename)
 	with open(docx_filename, 'rb') as f:
 		st.download_button('Download Trankript (*.docx)', f, file_name=docx_filename)
+		
+	# generate subtitles
+	if is_video:
+		webvtt_filename = original_file_name + ".vtt"
+		t.write_webvtt(sentences_with_speakers, webvtt_filename)
+		with open(webvtt_filename, 'rb') as f1:
+			st.download_button('Download subtitles (*.vtt)', f1, file_name=webvtt_filename)
 	st.balloons()
 	
 			
 if __name__ == '__main__':
 	logger.remove()
-	logger.add(sys.stderr, level="TRACE")
+	logger.add(sys.stderr, level="INFO")
 	
 	st.title("Meety - Herzlichen Willkommen!")
 	st.header("AI-basierte Audiotranskription für Web-Konferenzen")
@@ -460,7 +490,7 @@ if __name__ == '__main__':
 	with open("demo.mp3", 'rb') as f:
 		st.download_button('Download demo.mp3', f, file_name="demo.mp3")
 		
-	uploaded_file = st.file_uploader("Wählen eine Audiodatei", type=['wav', 'mp3', 'm4a'])
+	uploaded_file = st.file_uploader("Wählen eine Audiodatei", type=['wav', 'mp3', 'm4a', 'mp4'])
 	if uploaded_file is not None:
 
 		# store file as temp file
